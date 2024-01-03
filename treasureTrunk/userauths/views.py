ac912e54113bec3dc31e85c1ab1f7c1a079703be
SinhaAmrit <purpleplots@gmail.com>
2024-01-03 13:23:50 +0530

    feat-user-registration-system

--- treasureTrunk/userauths/views.py
+++ treasureTrunk/userauths/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from userauths.forms import UserRegistrationForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hey {username}, account created successfully!")
+            new_user = authenticate(
+                username=form.cleaned_data["email"],
+                password=form.cleaned_data["password1"],
+            )
+            login(request, new_user)
+            return redirect("core:index")
+    else:
+        form = UserRegistrationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "userauths/sign-up.html", context)
