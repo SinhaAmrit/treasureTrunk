b170110c772fd71c404725ef4bd46ee7d952524f
SinhaAmrit <purpleplots@gmail.com>
2024-01-04 22:16:58 +0530

    user login system

--- treasureTrunk/userauths/views.py
+++ treasureTrunk/userauths/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from userauths.forms import UserRegistrationForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 def register_view(request):
@@ -23,3 +26,25 @@ def register_view(request):
         "form": form,
     }
     return render(request, "userauths/sign-up.html", context)
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.info(request, "Hey {username}! You are already logged in.")
+        return redirect("core:index")
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"User with {email} does not exist")
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"You are logged in successfully")
+            return redirect("core:index")
+        else:
+            messages.warning(request, "User does not exist, create an account")
+    context = {}
+    return render(request, "userauths/sign-in.html", context)
