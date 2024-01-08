c93c48c746db532703eec69a81042b9cf9a89c60
SinhaAmrit <purpleplots@gmail.com>
2024-01-08 15:49:52 +0530

    added product display and detailed view

--- treasureTrunk/core/context_processor.py
+++ treasureTrunk/core/context_processor.py
@@ -13,6 +13,13 @@ from core.models import (
 
 def default(request):
     categories = Category.objects.all()
+    address = None
+    if request.user.is_authenticated:
+        try:
+            address = Address.objects.get(user=request.user)
+        except Address.DoesNotExist:
+            pass
     return {
         "categories": categories,
+        "address": address,
     }
--- treasureTrunk/core/urls.py
+++ treasureTrunk/core/urls.py
@@ -3,6 +3,7 @@ from core.views import (
     category_list_view,
     category_product_list_view,
     index,
+    product_detail_view,
     product_list_view,
     vendor_detail_view,
     vendor_list_view,
@@ -15,10 +16,11 @@ urlpatterns = [
     path("", index, name="index"),
     # Products
     path("products/", product_list_view, name="product-list"),
+    path("product/<pid>/", product_detail_view, name="product-detail"),
     # Categories
     path("categories/", category_list_view, name="category-list"),
-    path("category/<cid>", category_product_list_view, name="category-product-list"),
+    path("category/<cid>/", category_product_list_view, name="category-product-list"),
     # Vendors
     path("vendors/", vendor_list_view, name="vendor-list"),
-    path("vendor/<vid>", vendor_detail_view, name="vendor-detail"),
+    path("vendor/<vid>/", vendor_detail_view, name="vendor-detail"),
 ]
